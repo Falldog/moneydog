dd9ef0c80456c92a185c2aecfab4fbe7686b3cac
falldog7@gmail.com <falldog7@gmail.com@2210df80-94fd-11de-8746-af773c80d763>
2009-08-30 03:29:42 +0000

    [What] Add the HAP project file.
    git-svn-id: https://moneydog.googlecode.com/svn/trunk@5 2210df80-94fd-11de-8746-af773c80d763

---
+++ root.py
@@ -0,0 +1,29 @@
+###########################################################
+# Startup the moneydog script
+# the initial file path:
+# - MoneyDog
+#  - trunk
+#  - branches
+#  - google_appengine
+###########################################################
+import os
+import sys
+
+cwd = os.path.abspath('.')
+
+PROJECT_DIR   = cwd
+GAE_DIR       = os.path.join(cwd,     '../google_appengine')
+dev_appserver = os.path.join(GAE_DIR, 'dev_appserver.py'   )
+
+#change the global variable for execute dev_appserver file.
+my_globals = globals()
+my_globals['__file__'] = dev_appserver
+
+#sys.argv = [dev_appserver, 'moneydog-tw']
+sys.argv[0] = dev_appserver
+
+#change current working dir
+os.chdir( GAE_DIR )
+
+#execute GAE
+execfile(dev_appserver, my_globals)
