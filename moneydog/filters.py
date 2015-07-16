787cfaba9e660345c0a48576a60e795d2e3f6aaf
Falldog <falldog@hardcoretech.co>
2015-07-16 23:18:52 +0800

    add date picker support

--- moneydog/filters.py
+++ moneydog/filters.py
@@ -1,6 +1,8 @@
 from moneydog import app
 from moneydog.models import str2category, category2str
 
+from datetime import date
+
 
 app.jinja_env.filters['str2category'] = str2category
 app.jinja_env.filters['category2str'] = category2str
@@ -8,4 +10,10 @@ app.jinja_env.filters['category2str'] = category2str
 @app.template_filter('reverse')
 def reverse_filter(s):
     return s[::-1]
+
+@app.template_filter('format_date')
+def format_date(obj):
+    if obj and isinstance(obj, date):
+        return obj.strftime('%Y-%m-%d')
+    return None
 
