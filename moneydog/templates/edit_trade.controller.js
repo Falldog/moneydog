a5f075d42c9ad763250f6d3836b565f5c5839101
Falldog <falldog@hardcoretech.co>
2015-08-23 12:31:34 +0800

    refine add trade page 1. use angularjs to handle add function, 2. use angular-datepicker rather than bootstrap datepicker

---
+++ moneydog/templates/edit_trade.controller.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+'use strip'
+
+angular.module('moneydogApp')
+  .controller('Controller', [function() {
+    var vm = this;
+    vm.date = '{{item.date|format_date}}';
+  }]);
+
+</script>
